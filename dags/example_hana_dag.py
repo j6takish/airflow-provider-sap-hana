027eb6ace5b78d05bb1d7a094034483eb8fb0ee0
Jonathan Takish <jonathan.takish@gmail.com>
2024-12-29 15:45:25 -0500

    Create SapHanaHook
    - Override 'get_conn', 'set_autocommit', 'get_autocommit' with methods that will work with the hana cursor package.
    
    - Override the 'sqlalchemy_url' and 'inspector' properties to enable SQLAlchemy integration via the sqlalchemy-hana package.
    
    - Override '_make_common_data_structure' to make HANA ResultRow objects into a common format.
    
    - Create the '_make_resultrow_cell_serializable' and '_make_resultrow_common' methods to convert the hdbcli ResultRow class into a common data structure.
    
    - Create 'get_table_primary_key' method.
    
    - Create README.md to document how to use the package.
    
    - Create LICENSE containing the Apache License 2.0.
    
    - Create example DAG to demonstrate how the package can be used.
    
    - Create docker-compose.yaml and Dockerfile to enable users to build the environment to test out the package.

---
+++ dags/example_hana_dag.py
@@ -0,0 +1,128 @@
+from __future__ import annotations
+
+import csv
+from datetime import datetime
+
+from airflow.decorators import dag, task
+from airflow.operators.empty import EmptyOperator
+from airflow.providers.common.sql.operators.sql import BranchSQLOperator, SQLExecuteQueryOperator
+from faker import Faker
+from faker.providers import automotive, person
+
+from airflow_provider_sap_hana.hooks.hana import SapHanaHook
+
+
+@dag(
+    dag_id="example_hana_dag",
+    start_date=datetime(2024, 12, 20),
+    schedule_interval="@once",
+    max_active_runs=1,
+    catchup=False,
+)
+def example_hana_dag():
+    check_table_exists = BranchSQLOperator(
+        task_id="check_table_exists",
+        conn_id="hana_default",
+        follow_task_ids_if_false=["create_table"],
+        follow_task_ids_if_true=["do_nothing"],
+        sql="""
+        SELECT COUNT(*)
+        FROM sys.tables
+        WHERE
+            schema_name = 'AIRFLOW'
+            AND table_name = 'FAKE_VEHICLE_REGISTRATIONS';""",
+    )
+
+    create_table = SQLExecuteQueryOperator(
+        task_id="create_table",
+        conn_id="hana_default",
+        handler=None,
+        sql="""
+        CREATE TABLE airflow.fake_vehicle_registrations (
+            vin NVARCHAR(17),
+            owner_name_first NVARCHAR(30),
+            owner_name_last NVARCHAR(30),
+            address NVARCHAR(100),
+            city NVARCHAR(30),
+            state NVARCHAR(2),
+            postal_code NVARCHAR(20),
+            country NVARCHAR(2),
+            created_at TIMESTAMP,
+            PRIMARY KEY (vin)
+          );""",
+    )
+
+    do_nothing = EmptyOperator(task_id="do_nothing")
+
+    @task(trigger_rule="none_failed_min_one_success")
+    def create_fake_data():
+        fake = Faker()
+        fake.add_provider(automotive)
+        fake.add_provider(person)
+
+        with open("/tmp/fake_data.csv", mode="w", encoding="utf-8") as f:
+            writer = csv.writer(f)
+            for _ in range(100000):
+                vin = fake.vin()
+                owner_name_first = fake.first_name().upper()
+                owner_name_last = fake.last_name().upper()
+                street = fake.street_address().upper()
+                city = fake.city().upper()
+                state = fake.state_abbr().upper()
+                postal_code = fake.postalcode().upper()
+                country = "US"
+                created_at = fake.date_time_this_decade().isoformat()
+                writer.writerow(
+                    (
+                        vin,
+                        owner_name_first,
+                        owner_name_last,
+                        street,
+                        city,
+                        state,
+                        postal_code,
+                        country,
+                        created_at,
+                    )
+                )
+
+    @task(trigger_rule="none_failed_min_one_success")
+    def insert_into_hana():
+        with open("/tmp/fake_data.csv", encoding="utf-8") as f:
+            rows = list(csv.reader(f))
+
+        hook = SapHanaHook()
+        hook.insert_rows(
+            table="airflow.fake_vehicle_registrations",
+            rows=rows,
+            target_fields=[
+                "vin",
+                "owner_name_first",
+                "owner_name_last",
+                "address",
+                "city",
+                "state",
+                "postal_code",
+                "country",
+                "created_at",
+            ],
+            replace=True,
+            executemany=True,
+            autocommit=True,
+            commit_every=10000,
+        )
+
+    get_rows = SQLExecuteQueryOperator(
+        task_id="get_rows",
+        conn_id="hana_default",
+        show_return_value_in_logs=True,
+        sql="""
+        SELECT *
+        FROM airflow.fake_vehicle_registrations
+        LIMIT 100;""",
+    )
+
+    check_table_exists >> [create_table, do_nothing] >> create_fake_data() >> insert_into_hana() >> get_rows
+
+
+example_hana_dag()
---
+++ tests/hooks/test_sap_hana_hook.py
@@ -0,0 +1,258 @@
+from __future__ import annotations
+
+import datetime
+import json
+from unittest import mock
+
+import pytest
+from airflow.models.connection import Connection
+from hdbcli.resultrow import ResultRow
+
+from airflow_provider_sap_hana.hooks.hana import SapHanaHook
+
+
+@pytest.fixture
+def db_hook():
+    def _db_hook(schema_override=False, extra=None):
+        connection = Connection(
+            conn_type="hana",
+            host="hanahost",
+            login="user",
+            password="pass123",
+            port=12345,
+            schema="hana_schema",
+        )
+        if extra:
+            connection.extra = extra
+        hook = SapHanaHook(schema="schema_override" if schema_override else None)
+        hook.get_connection = mock.Mock(return_value=connection)
+        return hook
+
+    return _db_hook
+
+
+class TestSapHanaHookConn:
+    @pytest.mark.parametrize(
+        "schema_override, expected_uri",
+        [
+            (True, "hana://user:pass123@hanahost:12345/schema_override"),
+            (False, "hana://user:pass123@hanahost:12345/hana_schema"),
+        ],
+    )
+    def test_get_uri(self, schema_override, expected_uri, db_hook):
+        hook = db_hook(schema_override=schema_override)
+        uri = hook.get_uri()
+        assert uri == expected_uri
+
+    @pytest.mark.parametrize(
+        "extra, expected_uri",
+        [
+            (
+                '{"nodeConnectTimeout": "1000"}',
+                "hana://user:pass123@hanahost:12345/hana_schema?nodeConnectTimeout=1000",
+            ),
+            (
+                '{"packetSizeLimit": "1073741823"}',
+                "hana://user:pass123@hanahost:12345/hana_schema?packetSizeLimit=1073741823",
+            ),
+            (
+                '{"prefetch": "true", "cursorHoldabilityType": "rollback"}',
+                "hana://user:pass123@hanahost:12345/hana_schema?prefetch=true&cursorHoldabilityType=rollback",
+            ),
+        ],
+    )
+    def test_uri_with_extra(self, extra, expected_uri, db_hook):
+        hook = db_hook(extra=extra)
+        uri = hook.get_uri()
+        assert uri == expected_uri
+
+    @pytest.mark.parametrize(
+        "schema_override, expected_sa_url",
+        [
+            (False, "hana+hdbcli://user:pass123@hanahost:12345/hana_schema"),
+            (True, "hana+hdbcli://user:pass123@hanahost:12345/schema_override"),
+        ],
+    )
+    def test_sqlalchemy_url(self, schema_override, expected_sa_url, db_hook):
+        hook = db_hook(schema_override=schema_override)
+        sa_url = hook.sqlalchemy_url
+        assert str(sa_url) == expected_sa_url
+
+    @pytest.mark.parametrize(
+        "schema_override, called_with_database",
+        [
+            (False, "hana_schema"),
+            (True, "schema_override"),
+        ],
+    )
+    @mock.patch("airflow_provider_sap_hana.hooks.hana.hdbcli.dbapi.connect")
+    def test_get_conn(self, mock_connect, schema_override, called_with_database, db_hook):
+        hook = db_hook(schema_override=schema_override)
+        hook.get_conn()
+        mock_connect.assert_called_once_with(
+            address="hanahost", user="user", password="pass123", port=12345, databasename=called_with_database
+        )
+
+    @pytest.mark.parametrize(
+        "extra, called_with_extra",
+        [
+            ('{"nodeConnectTimeout": "1000"}', {"nodeConnectTimeout": "1000"}),
+            ('{"packetSizeLimit": "1073741823"}', {"packetSizeLimit": "1073741823"}),
+            (
+                '{"prefetch": "true", "cursorHoldabilityType": "rollback"}',
+                {"prefetch": "true", "cursorHoldabilityType": "rollback"},
+            ),
+        ],
+    )
+    @mock.patch("airflow_provider_sap_hana.hooks.hana.hdbcli.dbapi.connect")
+    def test_get_conn_with_extra(self, mock_connect, extra, called_with_extra, db_hook):
+        hook = db_hook(extra=extra)
+        hook.get_conn()
+        mock_connect.assert_called_once_with(
+            address="hanahost",
+            user="user",
+            password="pass123",
+            port=12345,
+            databasename="hana_schema",
+            **called_with_extra,
+        )
+
+    @pytest.mark.parametrize(
+        "is_autocommit_set, expected",
+        [
+            (True, True),
+            (False, False),
+        ],
+    )
+    @mock.patch("airflow_provider_sap_hana.hooks.hana.hdbcli.dbapi.connect")
+    def test_get_autocommit(self, mock_connect, is_autocommit_set, expected, db_hook):
+        mock_getautocommit = mock_connect.return_value.getautocommit
+        mock_getautocommit.return_value = is_autocommit_set
+        hook = db_hook()
+        conn = hook.get_conn()
+        autocommit = hook.get_autocommit(conn)
+        mock_getautocommit.assert_called_once()
+        assert autocommit == expected
+
+    @pytest.mark.parametrize("autocommit", [True, False])
+    @mock.patch("airflow_provider_sap_hana.hooks.hana.hdbcli.dbapi.connect")
+    def test_set_autocommit(self, mock_connect, autocommit, db_hook):
+        mock_setautocommit = mock_connect.return_value.setautocommit
+        hook = db_hook()
+        conn = hook.get_conn()
+        hook.set_autocommit(conn, autocommit)
+        mock_setautocommit.assert_called_once_with(autocommit)
+
+
+class TestSapHanaHook:
+    @pytest.fixture
+    def resultrows(self):
+        epoch_ts = datetime.datetime(
+            year=1970, month=1, day=1, hour=0, minute=0, second=0, microsecond=123456
+        )
+        epoch_ts_add_day = epoch_ts + datetime.timedelta(days=1)
+        epoch_ts_add_week = epoch_ts + datetime.timedelta(days=7)
+        column_names = ("MOCK_STRING", "MOCK_INT", "MOCK_FLOAT", "MOCK_DATETIME", "MOCK_NONE")
+        return [
+            ResultRow(column_names=column_names, column_values=("test123", 123, 123.00, epoch_ts, None)),
+            ResultRow(
+                column_names=column_names, column_values=("test456", 456, 456.00, epoch_ts_add_day, None)
+            ),
+            ResultRow(
+                column_names=column_names, column_values=("test789", 789, 789.00, epoch_ts_add_week, None)
+            ),
+        ]
+
+    @pytest.fixture
+    def cursor(self, resultrows):
+        cur = mock.MagicMock(rowcount=-1)
+        cur.fetchone = mock.Mock(return_value=resultrows[0])
+        cur.fetchall = mock.Mock(return_value=resultrows)
+        return cur
+
+    @pytest.fixture
+    def conn(self, cursor):
+        conn = mock.Mock()
+        conn.cursor.return_value = cursor
+        return conn
+
+    def test_resultrow_not_serializable(self, cursor):
+        result = cursor.fetchone()
+        with pytest.raises(TypeError, match="not JSON serializable"):
+            json.dumps(result)
+
+    @pytest.mark.parametrize(
+        "result_index, expected_type",
+        [
+            (0, str),
+            (1, int),
+            (2, float),
+            (3, str),
+            (4, type(None)),
+        ],
+    )
+    def test_make_resultrow_cell_serializable(self, result_index, expected_type, cursor, db_hook):
+        hook = db_hook()
+        result = cursor.fetchone()
+        cell = result[result_index]
+        serialized_cell = hook._make_resultrow_cell_serializable(cell)
+        assert isinstance(serialized_cell, expected_type)
+
+    def test_make_resultrow_common(self, cursor, db_hook):
+        hook = db_hook()
+        result = cursor.fetchone()
+        common_result = hook._make_resultrow_common(result)
+        expected_result = ("test123", 123, 123.00, "1970-01-01T00:00:00.123456", None)
+        assert common_result == expected_result
+
+    @pytest.mark.parametrize(
+        "handler, expected_data_structure",
+        [
+            ("fetchone", ("test123", 123, 123.00, "1970-01-01T00:00:00.123456", None)),
+            (
+                "fetchall",
+                [
+                    ("test123", 123, 123.00, "1970-01-01T00:00:00.123456", None),
+                    ("test456", 456, 456.00, "1970-01-02T00:00:00.123456", None),
+                    ("test789", 789, 789.00, "1970-01-08T00:00:00.123456", None),
+                ],
+            ),
+        ],
+    )
+    def test_make_common_data_structure(self, handler, expected_data_structure, cursor, db_hook):
+        hook = db_hook()
+        result = getattr(cursor, handler)()
+        common_result = hook._make_common_data_structure(result)
+        assert common_result == expected_data_structure
+
+    @pytest.mark.parametrize("handler", ["fetchone", "fetchall"])
+    def test_common_data_structure_is_serializable(self, handler, cursor, db_hook):
+        hook = db_hook()
+        result = getattr(cursor, handler)()
+        common_result = hook._make_common_data_structure(result)
+        json.dumps(common_result)
+
+    @pytest.mark.parametrize(
+        "get_records_return_val, expected_pks",
+        [
+            ([("TEST_COLUMN",)], ["TEST_COLUMN"]),
+            ([("TEST_COLUMN",), ("TEST_COLUMN2",)], ["TEST_COLUMN", "TEST_COLUMN2"]),
+            (None, None),
+        ],
+    )
+    @mock.patch("airflow_provider_sap_hana.hooks.hana.DbApiHook.get_records")
+    def test_table_get_primary_key(self, mock_get_records, get_records_return_val, expected_pks, db_hook):
+        hook = db_hook()
+        table = "SYS.TEST_TABLE"
+        sql = """
+        SELECT column_name
+        FROM SYS.CONSTRAINTS
+        WHERE
+            is_primary_key = 'TRUE'
+            AND schema_name = ?
+            AND table_name = ?
+        """
+        mock_get_records.return_value = get_records_return_val
+        pks = hook.get_table_primary_key(table)
+        mock_get_records.assert_called_once_with(sql=sql, parameters=("SYS", "TEST_TABLE"))
+        assert pks == expected_pks
